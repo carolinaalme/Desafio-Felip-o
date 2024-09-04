a23036a8dc60282a161070d1d1b702d373739f7c
carolinaalme <carolinaalmeida189@gmail.com>
2024-09-04 15:19:56 -0300

    segundo comando

--- index.js
+++ index.js
@@ -4,12 +4,25 @@ let nivel = "";
 
 switch (true){
     case (xp < 1000):
-     nivel = "ferro";
+    nivel = "ferro";
     break;
     
-    case (xp <=2000):
+    case (xp <= 2000):
     nivel = "Bronze";
     break;
+
+    case (xp <= 5000):
+    nivel = "Prata";
+    break;
+
+    case (xp <= 7000):
+    nivel = "Ouro";
+    break;
+
+    case (xp <= 8000):
+    nivel = "Platina";
+    break;
+    
 
 
 }
