b3acf5073c7f16b955b2505d9d7f5d000b29bc3c
carolinaalme <carolinaalmeida189@gmail.com>
2024-09-04 14:13:07 -0300

    primeiro commit

---
+++ index.js
@@ -0,0 +1,15 @@
+let nome = "felipão, o Invencivel";
+let xp = 7000
+let nivel = "";
+
+switch (true){
+    case (xp < 1000):
+     nivel = "ferro";
+    break;
+    
+    case (xp <=2000):
+    nivel = "Bronze";
+    break;
+
+
+}
